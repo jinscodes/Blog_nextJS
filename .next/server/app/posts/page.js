3e24ea57779ce702d52f1502d9c7f1c63970dde1
{jinsung} <jayhan0215@gmail.com>
2023-08-01 00:15:08 +0900

    Add Posts' slug page
    - not yet styling

---
+++ .next/server/app/posts/page.js
@@ -0,0 +1,459 @@
+(() => {
+var exports = {};
+exports.id = 991;
+exports.ids = [991];
+exports.modules = {
+
+/***/ 8038:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react");
+
+/***/ }),
+
+/***/ 8704:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-dom/server-rendering-stub");
+
+/***/ }),
+
+/***/ 7897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-server-dom-webpack/client");
+
+/***/ }),
+
+/***/ 6786:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react/jsx-runtime");
+
+/***/ }),
+
+/***/ 1844:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/app-render/get-segment-param");
+
+/***/ }),
+
+/***/ 6624:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/future/helpers/interception-routes");
+
+/***/ }),
+
+/***/ 7085:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/app-router-context");
+
+/***/ }),
+
+/***/ 1830:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/get-img-props");
+
+/***/ }),
+
+/***/ 199:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hash");
+
+/***/ }),
+
+/***/ 6864:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head");
+
+/***/ }),
+
+/***/ 9569:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hooks-client-context");
+
+/***/ }),
+
+/***/ 2210:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config");
+
+/***/ }),
+
+/***/ 5359:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config-context");
+
+/***/ }),
+
+/***/ 7160:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router-context");
+
+/***/ }),
+
+/***/ 893:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix");
+
+/***/ }),
+
+/***/ 2336:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/format-url");
+
+/***/ }),
+
+/***/ 4490:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/handle-smooth-scroll");
+
+/***/ }),
+
+/***/ 8735:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-bot");
+
+/***/ }),
+
+/***/ 120:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-local-url");
+
+/***/ }),
+
+/***/ 162:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-path");
+
+/***/ }),
+
+/***/ 3750:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash");
+
+/***/ }),
+
+/***/ 982:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/resolve-href");
+
+/***/ }),
+
+/***/ 9618:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/server-inserted-html");
+
+/***/ }),
+
+/***/ 8423:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils");
+
+/***/ }),
+
+/***/ 8658:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils/warn-once");
+
+/***/ }),
+
+/***/ 3292:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs/promises");
+
+/***/ }),
+
+/***/ 1017:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("path");
+
+/***/ }),
+
+/***/ 3386:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   GlobalError: () => (/* reexport default from dynamic */ next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0___default.a),
+/* harmony export */   __next_app__: () => (/* binding */ __next_app__),
+/* harmony export */   originalPathname: () => (/* binding */ originalPathname),
+/* harmony export */   pages: () => (/* binding */ pages),
+/* harmony export */   tree: () => (/* binding */ tree)
+/* harmony export */ });
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2673);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9419);
+/* harmony import */ var next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
+/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__) if(["default","tree","pages","GlobalError","originalPathname","__next_app__"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__[__WEBPACK_IMPORT_KEY__]
+/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
+
+    const tree = {
+        children: [
+        '',
+        {
+        children: [
+        'posts',
+        {
+        children: ['__PAGE__', {}, {
+          page: [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 831)), "/Users/hanjinsung/Desktop/blog_nextJS/src/app/posts/page.tsx"],
+          
+        }]
+      },
+        {
+          
+          
+        }
+      ]
+      },
+        {
+          'layout': [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8231)), "/Users/hanjinsung/Desktop/blog_nextJS/src/app/layout.tsx"],
+          
+        }
+      ]
+      }.children;
+    const pages = ["/Users/hanjinsung/Desktop/blog_nextJS/src/app/posts/page.tsx"];
+    
+    const originalPathname = "/posts/page"
+    const __next_app__ = {
+      require: __webpack_require__,
+      // all modules are in the entry chunk, so we never actually need to load chunks in webpack
+      loadChunk: () => Promise.resolve()
+    }
+
+    
+  
+
+/***/ }),
+
+/***/ 4515:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 1746))
+
+/***/ }),
+
+/***/ 1746:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  FilterablePosts: () => (/* binding */ FilterablePosts)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(6786);
+// EXTERNAL MODULE: ./src/components/Categories/Categories.module.scss
+var Categories_module = __webpack_require__(650);
+var Categories_module_default = /*#__PURE__*/__webpack_require__.n(Categories_module);
+;// CONCATENATED MODULE: ./src/components/Categories/Categories.tsx
+
+
+const Categories = ({ categories, selected, onClick })=>{
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: (Categories_module_default()).div,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                children: "Category"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("ul", {
+                children: categories.map((category)=>/*#__PURE__*/ jsx_runtime_.jsx("li", {
+                        onClick: ()=>onClick(category),
+                        className: category === selected ? (Categories_module_default()).li : "",
+                        children: category
+                    }, category))
+            })
+        ]
+    });
+};
+
+// EXTERNAL MODULE: external "next/dist/compiled/react"
+var react_ = __webpack_require__(8038);
+// EXTERNAL MODULE: ./src/components/PostCard/PostCard.tsx
+var PostCard = __webpack_require__(9152);
+// EXTERNAL MODULE: ./src/components/FilterablePosts/FilterablePosts.module.scss
+var FilterablePosts_module = __webpack_require__(7535);
+var FilterablePosts_module_default = /*#__PURE__*/__webpack_require__.n(FilterablePosts_module);
+;// CONCATENATED MODULE: ./src/components/FilterablePosts/FilterablePosts.tsx
+/* __next_internal_client_entry_do_not_use__ FilterablePosts auto */ 
+
+
+
+
+const ALL_POSTS = "All Posts";
+const FilterablePosts = ({ posts, categories })=>{
+    const [selected, setSelected] = (0,react_.useState)(ALL_POSTS);
+    const filtered = selected === ALL_POSTS ? posts : posts.filter((post)=>post.category === selected);
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("section", {
+        className: (FilterablePosts_module_default()).section,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("ul", {
+                className: (FilterablePosts_module_default()).ul,
+                children: filtered.map((post)=>/*#__PURE__*/ jsx_runtime_.jsx("li", {
+                        className: (FilterablePosts_module_default()).li,
+                        children: /*#__PURE__*/ jsx_runtime_.jsx(PostCard/* default */.Z, {
+                            post: post
+                        })
+                    }, post.path))
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Categories, {
+                categories: [
+                    ALL_POSTS,
+                    ...categories
+                ],
+                selected: selected,
+                onClick: setSelected
+            })
+        ]
+    });
+};
+
+
+/***/ }),
+
+/***/ 650:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"div": "Categories_div__CCqVF",
+	"li": "Categories_li__MkYrN"
+};
+
+
+/***/ }),
+
+/***/ 7535:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"section": "FilterablePosts_section__IY2Oh",
+	"ul": "FilterablePosts_ul__u_CDo"
+};
+
+
+/***/ }),
+
+/***/ 831:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ page)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(6786);
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-flight-loader/module-proxy.js
+var module_proxy = __webpack_require__(1313);
+;// CONCATENATED MODULE: ./src/components/FilterablePosts/FilterablePosts.tsx
+
+const proxy = (0,module_proxy.createProxy)(String.raw`/Users/hanjinsung/Desktop/blog_nextJS/src/components/FilterablePosts/FilterablePosts.tsx`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule, $$typeof } = proxy;
+const __default__ = proxy.default;
+
+const e0 = proxy["FilterablePosts"];
+
+// EXTERNAL MODULE: ./src/service/posts.ts
+var service_posts = __webpack_require__(8957);
+;// CONCATENATED MODULE: ./src/app/posts/page.tsx
+
+
+
+const Posts = async ()=>{
+    const posts = await (0,service_posts/* getAllPosts */.Bd)();
+    const categories = [
+        ...new Set(posts.map((post)=>post.category))
+    ];
+    return /*#__PURE__*/ jsx_runtime_.jsx("div", {
+        style: {
+            paddingTop: "30px"
+        },
+        children: /*#__PURE__*/ jsx_runtime_.jsx(e0, {
+            posts: posts,
+            categories: categories
+        })
+    });
+};
+/* harmony default export */ const page = (Posts);
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [848,121,380,152], () => (__webpack_exec__(3386)));
+module.exports = __webpack_exports__;
+
+})();
