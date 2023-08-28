58b15036bf21b0cd366875aa2d283faec772beff
{jinsung} <jayhan0215@gmail.com>
2023-08-28 09:17:41 +0900

    build

---
+++ .next/server/chunks/421.js
@@ -0,0 +1,61 @@
+exports.id = 421;
+exports.ids = [421];
+exports.modules = {
+
+/***/ 81475:
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({
+    value: true
+}));
+0 && (0);
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        enumerable: true,
+        get: all[name]
+    });
+}
+_export(exports, {
+    default: function() {
+        return _default;
+    },
+    unstable_getImgProps: function() {
+        return unstable_getImgProps;
+    }
+});
+const _interop_require_default = __webpack_require__(95967);
+const _getimgprops = __webpack_require__(1830);
+const _warnonce = __webpack_require__(98658);
+const _imagecomponent = __webpack_require__(63912);
+const _imageloader = /*#__PURE__*/ _interop_require_default._(__webpack_require__(96277));
+const unstable_getImgProps = (imgProps)=>{
+    (0, _warnonce.warnOnce)("Warning: unstable_getImgProps() is experimental and may change or be removed at any time. Use at your own risk.");
+    const { props } = (0, _getimgprops.getImgProps)(imgProps, {
+        defaultLoader: _imageloader.default
+    });
+    for (const [key, value] of Object.entries(props)){
+        if (value === undefined) {
+            delete props[key];
+        }
+    }
+    return {
+        props
+    };
+};
+const _default = _imagecomponent.Image; //# sourceMappingURL=image-external.js.map
+
+
+/***/ }),
+
+/***/ 48421:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__(81475)
+
+
+/***/ })
+
+};
+;
