842fbe4a5e9c496fb5e4a73273705e5020fbebde
{jinsung} <jayhan0215@gmail.com>
2023-10-01 21:47:58 +0900

    Post - Image

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getPostData } from "@/service/posts";
 import st from "./slug.module.scss";
 
 type Props = {
@@ -9,26 +10,13 @@ type Props = {
 const PostPage = async ({ params: { slug } }: Props) => {
   // const post = await getPostData(slug);
   // const { title, path, next, prev } = post;
+  const post = await getPostData(slug);
+  const { title, path, next, prev } = post;
 
   return (
     <article className={st.article}>
+      <img src={`/images/posts/${path}.png`} alt={path} className={st.img} />
       {slug} page
-      {/* <div className={st.img_frame}>
-        <h1>{title}</h1>
-        <Image
-          src={`/images/posts/${path}.png`}
-          alt="path"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className={st.img}
-        />
-      </div>
-      <PostContent post={post} />
-      <section className={st.adjacent}>
-        {prev && <AdjacentPostCard post={prev} type="prev" />}
-        {next && <AdjacentPostCard post={next} type="next" />}
-      </section> */}
     </article>
   );
 };
