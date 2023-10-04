451db5e1978e70cb381ae61405dc294af51a063f
{jinsung} <jayhan0215@gmail.com>
2023-10-04 14:45:26 +0900

    new branch test

--- src/app/posts/[postPage]/[categorizedPostPage]/page.tsx
+++ src/app/posts/[postPage]/[categorizedPostPage]/page.tsx
@@ -1,10 +1,18 @@
+import { getPostData } from "@/service/posts";
+import { log } from "console";
+
 type Prop = {
   params: {
     categorizedPostPage: string;
   };
 };
 
-const CategorizedPostPage = ({ params: { categorizedPostPage } }: Prop) => {
+const CategorizedPostPage = async ({
+  params: { categorizedPostPage },
+}: Prop) => {
+  const file = await getPostData(categorizedPostPage);
+  log(file);
+
   return <div>slug2 page {categorizedPostPage}</div>;
 };
 
