157f3b133d00112fd91a1758a0266c5b72c346c5
{jinsung} <jayhan0215@gmail.com>
2023-10-23 09:39:17 +0900

    fix the bug (path)

--- src/components/Post/PostCard/PostCard.tsx
+++ src/components/Post/PostCard/PostCard.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import CategoryPill from "@/components/CategoryPill/CategoryPill";
-import ProfileAndId from "@/components/ProfileAndId/ProfileAndId";
-import { Post } from "@/service/posts";
+import { Post } from "service/posts";
+
+import CategoryPill from "components/CategoryPill/CategoryPill";
+import ProfileAndId from "components/ProfileAndId/ProfileAndId";
 
 import st from "./PostCard.module.scss";
 
