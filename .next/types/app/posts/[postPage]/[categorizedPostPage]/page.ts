f122430ed7db4b8d09735de23f83ebbf2f128fc2
{jinsung} <jayhan0215@gmail.com>
2023-10-26 17:55:05 +0900

    Posting: All-about-jsx

--- .next/types/app/posts/[postPage]/[categorizedPostPage]/page.ts
+++ .next/types/app/posts/[postPage]/[categorizedPostPage]/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/hanjinsung/Desktop/blog_nextJS/src/app/posts/[postPage]/[categorizedPostPage]/page.tsx
+// File: /Users/hanjinsung/Desktop/Blog_nextJS/src/app/posts/[postPage]/[categorizedPostPage]/page.tsx
 import * as entry from '../../../../../../src/app/posts/[postPage]/[categorizedPostPage]/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
