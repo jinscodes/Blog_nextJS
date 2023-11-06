b824e86a46216aeef5bd896f6a660715acc3c6b1
{jinsung} <jayhan0215@gmail.com>
2023-11-06 13:50:10 +0900

    Responsive Tablets HOME and NAV

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -2,6 +2,7 @@ import Categories from "components/Home/Categories/Categories";
 import Gif from "components/Home/Gif/Gif";
 import OpenSources from "components/Home/OpenSources/OpenSources";
 import RecentPosts from "components/Home/RecentPosts/RecentPosts";
+
 import st from "./Home.module.scss";
 
 const Home = async () => {
