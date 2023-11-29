b9f882703b72c5af7e1445f6aba1aa0b7085b951
{jinsung} <jayhan0215@gmail.com>
2023-11-29 15:16:51 +0900

    fix the responsive styles

--- src/components/Home/Gif/Gif.tsx
+++ src/components/Home/Gif/Gif.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 
+import closeQuote from "assets/svg/CloseQuote.svg";
 import closeBracket from "assets/svg/Close_bracket.svg";
 import downArrow from "assets/svg/Down_arrow.svg";
 import heart from "assets/svg/Heart.svg";
+import openQuote from "assets/svg/OpenQuote.svg";
 import openBracket from "assets/svg/Open_bracket.svg";
 import st from "./Gif.module.scss";
 
@@ -15,9 +17,27 @@ const Gif = () => {
             <Image className={st.heart} src={heart} alt="heart" />
             <h1>
               FALL INTO THE
-              <Image src={openBracket} alt="open_bracket" />
+              <Image
+                src={openBracket}
+                alt="open_bracket"
+                className={st.bracket}
+              />
+              <Image
+                src={openQuote}
+                alt="open_bracket"
+                className={`${st.svg_quote} ${st.open_quote}`}
+              />
               HELLO WORLD
-              <Image src={closeBracket} alt="Close_bracket" />
+              <Image
+                src={closeBracket}
+                alt="Close_bracket"
+                className={st.bracket}
+              />
+              <Image
+                src={closeQuote}
+                alt="Close_bracket"
+                className={`${st.svg_quote} ${st.close_quote}`}
+              />
             </h1>
             <p>LOREM IPSUM DOLOR SIT AMET</p>
             <div className={st.arrow_container}>
