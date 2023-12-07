1c93e71f2ff6c7d044469edbdbd04f296351696f
{jinsung} <jayhan0215@gmail.com>
2023-12-07 09:57:48 +0900

    build

--- .next/server/app/posts/[postPage]/[categorizedPostPage]/page.js
+++ .next/server/app/posts/[postPage]/[categorizedPostPage]/page.js
@@ -895,7 +895,7 @@ __webpack_require__.r(__webpack_exports__);
 var __webpack_require__ = require("../../../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [160,543,798,508], () => (__webpack_exec__(71200)));
+var __webpack_exports__ = __webpack_require__.X(0, [160,543,374,508], () => (__webpack_exec__(71200)));
 module.exports = __webpack_exports__;
 
 })();
