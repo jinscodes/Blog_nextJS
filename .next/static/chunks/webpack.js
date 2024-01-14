5c329679cfdf5875b94fd51be0fb59ccc202ec16
{jinsung} <jayhan0215@gmail.com>
2024-01-14 18:14:31 +0900

    responsive mobile posting page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf00455a502fbe46"; }
+/******/ 		__webpack_require__.h = function() { return "c57a034a86e61546"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
