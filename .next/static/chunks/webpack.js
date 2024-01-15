88c04e8246741382dafa56bb18f70ad2a5bc1ca8
{jinsung} <jayhan0215@gmail.com>
2024-01-15 12:49:33 +0900

    responsive mobile: style fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae8ac5a897268391"; }
+/******/ 		__webpack_require__.h = function() { return "f57eefff1b8e4df2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
